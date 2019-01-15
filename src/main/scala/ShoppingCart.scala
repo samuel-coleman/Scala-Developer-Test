edc02bdd0d7be4d0355e4ba2d63c3cb0c5956d04
Samuel Coleman <samuelwjcoleman@gmail.com>
2019-01-15 11:43:48 +0000

    Step 1

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -1,3 +1,15 @@
 object ShoppingCart extends App{
 
+  def checkout(listFruit:List[String]): Double ={
+    var cost:Double = 0.00
+    for(i <- 0 to listFruit.length-1){
+      listFruit(i) match{
+        case "Apple" => cost += 0.25
+        case "Orange" => cost +=0.60
+      }
+    }
+    cost
+  }
+
+  println(s"Cost is: £${checkout(List("Apple","Orange"))}")
 }
